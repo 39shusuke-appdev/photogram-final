e0ec429152cf226da8c130e8f412ebe42d0258d3
39shusuke <ssakurai@chicagobooth.edu>
2023-02-27 21:58:09 +0000

    updated_3

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -12,4 +12,19 @@
 #  owner_id       :integer
 #
 class Photo < ApplicationRecord
+  has_many(:comments, {
+    :class_name => "Comment",
+    :foreign_key => "photo_id"
+  })
+
+  has_many(:likes, {
+    :class_name => "Like",
+    :foreign_key => "photo_id",
+    :dependent => :destroy
+  })
+
+  belongs_to(:owner, {
+    :class_name => "User"
+  })
+
 end
